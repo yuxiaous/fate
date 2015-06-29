39162c088c6c45de67542dfd8085d99b9ade932b
yuxiao <yuxiaous@gmail.com>
2015-06-30 01:22:32 +0800

    and game project

---
+++ frameworks/runtime-src/proj.android.andgame/src/com/fate/SdkManagerJni.java
@@ -0,0 +1,9 @@
+package com.fate;
+
+import android.app.Activity;
+import android.app.Application;
+
+public class SdkManagerJni {
+	public static Activity activity = null;
+	public static Application application = null;
+}
