8f709f49a65769dbc05df800e7456f133bfbb8d3
yuxiao <yuxiaous@gmail.com>
2015-06-30 18:06:46 +0800

    no message

--- frameworks/runtime-src/proj.android/src/com/fate/SdkManagerJni.java
+++
@@ -1,9 +0,0 @@
-package com.fate;
-
-import android.app.Activity;
-import android.app.Application;
-
-public class SdkManagerJni {
-	public static Activity activity = null;
-	public static Application application = null;
-}
