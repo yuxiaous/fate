bc83e9423dabab8dd2540249a3b35fad4d0cdfab
yuxiao <yuxiaous@gmail.com>
2015-07-07 18:25:57 +0800

    mm

--- frameworks/runtime-src/proj.android.common/targets/cmcc_mm/custom_step.py
+++ frameworks/runtime-src/proj.android.common/targets/cmcc_mm/custom_step.py
@@ -5,10 +5,10 @@ def handle_event(event, args):
 
     if event == "post-ndk-build":
         lib_path = "../../../../../Classes/sdk/mmbilling/android/armeabi"
-        shutil.copy(os.path.join(lib_path, "libcasdkjni.so"), "../../libs/armeabi/libcasdkjni.so")
-        shutil.copy(os.path.join(lib_path, "libcmcc_haze.so"), "../../libs/armeabi/libcmcc_haze.so")
-        shutil.copy(os.path.join(lib_path, "libcmcc_rusteze.so"), "../../libs/armeabi/libcmcc_rusteze.so")
-        shutil.copy(os.path.join(lib_path, "libidentifyapp.so"), "../../libs/armeabi/libidentifyapp.so")
+        shutil.copy(os.path.join(lib_path, "libcasdkjni.so"), "../../libs/armeabi")
+        shutil.copy(os.path.join(lib_path, "libcmcc_haze.so"), "../../libs/armeabi")
+        shutil.copy(os.path.join(lib_path, "libcmcc_rusteze.so"), "../../libs/armeabi")
+        shutil.copy(os.path.join(lib_path, "libidentifyapp.so"), "../../libs/armeabi")
         pass
 
 
