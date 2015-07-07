00579a75a0b4b4297c0adf0fcb3601b85162dc2b
yuxiao <yuxiaous@gmail.com>
2015-07-07 14:06:38 +0800

    target mm

--- custom_step_script.py
+++ custom_step_script.py
@@ -1,15 +1,26 @@
-import sys; 
-
-target_path = "frameworks/runtime-src/proj.android/target"
-custom_script = "custom_step"
-
-if not target_path in sys.path:
-    sys.path.append(target_path) 
-
-if not custom_script in sys.modules:
-    custom_step = __import__(custom_script)
-
+import sys 
+import os
 
 def handle_event(event, tp, args):
-    custom_step.handle_event(event, tp, args)
-    pass
+    # args: {
+    #     'ndk-build-mode': 'debug', 
+    #     'project-path': '/Users/yuxiao/Documents/workspace/fate/client', 
+    #     'build-mode': 'debug', 
+    #     'platform-project-path': '/Users/yuxiao/Documents/workspace/fate/client/frameworks/runtime-src/proj.android', 
+    #     'output-dir': '/Users/yuxiao/Documents/workspace/fate/client/runtime/android'
+    # }
+
+    if tp == "android":
+        target_path = os.path.join(args['platform-project-path'], "target")
+        if not os.path.exists(target_path):
+            print target_path + " not exists."
+            return
+
+        global custom
+        if not target_path in sys.path:
+            sys.path.append(target_path) 
+        if not "custom_step" in sys.modules:
+            custom = __import__("custom_step")
+
+        os.chdir(target_path)
+        custom.handle_event(event, args)
--- frameworks/runtime-src/proj.android.common/targets/cmcc_mm/custom_step.py
+++ frameworks/runtime-src/proj.android.common/targets/cmcc_mm/custom_step.py
@@ -1,7 +1,48 @@
+import shutil
+import os
 
-def handle_event(event, tp, args):
-    print "custom step"
-    print "event:"+event
-    print "tp:"+tp
+def merge_dir(from_dir, to_dir):
+    for parent, dirnames, filenames in os.walk(from_dir):
+        for dirname in  dirnames: 
+            dest_dir = os.path.join(to_dir, parent, dirname)
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+
+        for filename in filenames:
+            if filename == ".DS_Store":
+                continue
+            shutil.copy(os.path.join(parent, filename), os.path.join(to_dir, parent, filename))
+
+
+
+
+
+def handle_event(event, args):
+    print "event: "+event
     print args
+
+    if event == "pre-ndk-build":
+        shutil.copy2("jni/Application.mk", "../../jni/Application.mk")
+        pass
+
+    if event == "post-ndk-build":
+        lib_path = "../../../../../Classes/sdk/mmbilling/android/armeabi"
+        shutil.copy(os.path.join(lib_path, "libcasdkjni.so"), "../../libs/armeabi/libcasdkjni.so")
+        shutil.copy(os.path.join(lib_path, "libcmcc_haze.so"), "../../libs/armeabi/libcmcc_haze.so")
+        shutil.copy(os.path.join(lib_path, "libcmcc_rusteze.so"), "../../libs/armeabi/libcmcc_rusteze.so")
+        shutil.copy(os.path.join(lib_path, "libidentifyapp.so"), "../../libs/armeabi/libidentifyapp.so")
+        pass
+
+    if event == "post-copy-assets":
+        merge_dir("res", "../..")
+        pass
+
+    if event == "pre-ant-build":
+        shutil.copy("AndroidManifest.xml", "../../AndroidManifest.xml")
+        shutil.copy("build.xml", "../../build.xml")
+        merge_dir("src", "../..")
+        pass
+
+
     pass
+
