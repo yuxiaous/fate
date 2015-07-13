543353e5c6a55ca752b2ac1f7798beeb49e79d5d
yuxiao <yuxiaous@gmail.com>
2015-07-13 13:30:18 +0800

    target inherit

---
+++ frameworks/runtime-src/proj.android/targets/inherit/src/com/fate/SdkManagerJni.java
@@ -0,0 +1,14 @@
+package com.fate;
+
+import android.app.Activity;
+import android.app.Application;
+
+public class SdkManagerJni {
+	public static Activity activity = null;
+	public static Application application = null;
+
+	public static native void setAppActivity(Activity a);
+	public static native void activityOnCreate();
+	public static native void activityOnPause();
+	public static native void activityOnResume();
+}
---
+++ frameworks/runtime-src/proj.android/targets/inherit/src/org/cocos2dx/javascript/AppApplication.java
@@ -0,0 +1,20 @@
+package org.cocos2dx.javascript;
+
+import android.app.Application;
+import com.fate.SdkManagerJni;
+
+/**
+ * Created by yuxiao on 15/7/9.
+ */
+public class AppApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SdkManagerJni.application = this;
+
+        System.loadLibrary("KTPlay");
+        System.loadLibrary("KTAccountmanager");
+        System.loadLibrary("KTFriendship");
+        System.loadLibrary("KTLeaderboard");
+    }
+}
