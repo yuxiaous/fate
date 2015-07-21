27b7dc09675615512120bd4dcc1e84ed43854f99
yuxiao <yuxiaous@gmail.com>
2015-07-21 14:35:46 +0800

    ktplay

--- src/sdk_manager.js
+++ src/sdk_manager.js
@@ -12,9 +12,47 @@ var sdk_manager = {
         LOG("onSdkCommond name: {0}, cmd: {1}".format(name, cmd));
 
         if(name == "Ktplay") {
-
+            ktplay_helper.onSdkCommond(cmd);
         }
     }
 };
 
 jsb.SdkManager.setSdkCommandCallback(sdk_manager.onSdkCommond);
+
+
+
+var ktplay_helper = {
+    onSdkCommond: function(cmd) {
+        var cmds = cmd.split(";");
+        if(cmds[0] == "Rewards") {
+            var rewards = [];
+            _.each(cmds, function(cmd, i) {
+                if(i == 0) return;
+
+                var reward = cmd.split("=");
+                rewards.push({
+                    typeId: reward[0],
+                    value: Number(reward[1])
+                });
+            });
+            this.dispatchRewards(rewards);
+        }
+    },
+
+    dispatchRewards: function(rewards) {
+        LOG("dispatchRewards");
+        _.each(rewards, function(reward) {
+            var types = reward.typeId.split(".");
+            var config = configdb[types[0]][types[1]];
+            if(config == undefined) {
+                return;
+            }
+
+
+
+        });
+    }
+};
+
+
+
