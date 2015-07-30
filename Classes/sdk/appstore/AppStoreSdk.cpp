162e9ca2fa19fecc14651bb619391f5f66e19a02
yuxiao <yuxiaous@gmail.com>
2015-07-30 23:13:35 +0800

    appstore

---
+++ Classes/sdk/appstore/AppStoreSdk.cpp
@@ -0,0 +1,9 @@
+//
+//  AppStoreSdk.cpp
+//  fate
+//
+//  Created by yuxiao on 15/7/30.
+//
+//
+
+#include "AppStoreSdk.h"
---
+++ Classes/sdk/appstore/AppStoreSdk.h
@@ -0,0 +1,20 @@
+//
+//  AppStoreSdk.h
+//  fate
+//
+//  Created by yuxiao on 15/7/30.
+//
+//
+
+#ifndef __fate__AppStoreSdk__
+#define __fate__AppStoreSdk__
+
+#include <stdio.h>
+#include "Sdk.h"
+
+class AppStoreSdk : public Sdk, public SdkChargeProtocol
+{
+    
+};
+
+#endif /* defined(__fate__AppStoreSdk__) */
