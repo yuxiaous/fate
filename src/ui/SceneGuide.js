d9f8abc1e14d7ffb582261dcb05e8c8dbf10eff7
yuxiao <yuxiaous@gmail.com>
2015-08-11 20:07:01 +0800

    no message

---
+++ src/ui/SceneGuide.js
@@ -0,0 +1,11 @@
+/**
+ * Created by yuxiao on 15/8/10.
+ */
+
+
+var SceneGuide = SceneBase.extend({
+    ctor: function () {
+        this._super("factory", SceneBase.Type.GuideType);
+    }
+});
+
