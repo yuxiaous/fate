318568ba6e3f18e471158dd4f4bd4e668d64b5a2
yuxiao <yuxiaous@gmail.com>
2015-09-09 17:17:29 +0800

    qq

--- Classes/sdk/TencentMidas/TencentMidasSdk.cpp
+++ Classes/sdk/TencentMidas/TencentMidasSdk.cpp
@@ -7,34 +7,34 @@
 using namespace cocos2d;
 
 
-#define  CLASS_NAME "com/hdngame/fate/midas/TencentMidasSdkJni"
+#define  CLASS_NAME "com/hdngame/fate/qq/TencentMidasSdkJni"
 
 extern "C" {
-    int Java_com_hdngame_fate_midas_TencentMidasSdkJni_getLaunchType(JNIEnv *env, jobject thiz) {
+    int Java_com_hdngame_fate_qq_TencentMidasSdkJni_getLaunchType(JNIEnv *env, jobject thiz) {
         return 1;
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getAppSecretKey(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getAppSecretKey(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("111222333444555");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getOfferId(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getOfferId(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("1450000766");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getUserId(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getUserId(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("uin_20150909");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getUserKey(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getUserKey(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("skey");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getSessionId(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getSessionId(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("hy_gameid");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getSessionType(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getSessionType(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("st_dummy");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getPf(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getPf(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("huyu_m-2001-android");
     }
-    jstring Java_com_hdngame_fate_midas_TencentMidasSdkJni_getPfKey(JNIEnv *env, jobject thiz) {
+    jstring Java_com_hdngame_fate_qq_TencentMidasSdkJni_getPfKey(JNIEnv *env, jobject thiz) {
         return env->NewStringUTF("pfKey");
     }
 }
