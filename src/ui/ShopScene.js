4dc07686a9a3dfe9e5ec67e14b151d56fae24699
cj299 <sleifeei@126.com>
2015-09-09 19:05:54 +0800

    fix bug

--- src/ui/ShopScene.js
+++ src/ui/ShopScene.js
@@ -110,8 +110,8 @@ var ShopScene = ui.GuiSceneBase.extend({
     },
 
     _on_btn_back: function() {
-        ui.replaceScene(MainScene);
-        //this.popScene();
+        //ui.replaceScene(MainScene);
+        this.popScene();
     },
 
     refreshShopTabStatus: function() {
