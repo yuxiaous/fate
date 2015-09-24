96a16ddfa408d80124e7f920535ce5ef3ae29f72
yuxiao <yuxiaous@gmail.com>
2015-09-24 16:31:36 +0800

    kuwo

--- Classes/sdk/KuwoLoginSdk/KuwoLoginSdk.cpp
+++ Classes/sdk/KuwoLoginSdk/KuwoLoginSdk.cpp
@@ -6,9 +6,21 @@ using namespace cocos2d;
 
 #define  CLASS_NAME "com/hdngame/fate/kuwo/KuwoLoginSdkJni"
 
+extern "C" {
+    void KuwoLoginSdk_enterGame(const std::string &gameid)
+    {
+        cocos2d::log("KuwoLoginSdk_enterGame");
+
+        JniMethodInfo minfo;
+        if (JniHelper::getStaticMethodInfo(minfo, CLASS_NAME, "enterGame", "(Ljava/lang/String;)V")) {
+            jstring jgameid = minfo.env->NewStringUTF(gameid.c_str());
+            minfo.env->CallStaticVoidMethod(minfo.classID, minfo.methodID, jgameid);
+        }
+    }
+}
 
 void KuwoLoginSdk::activityOnCreate()
 {
-
+    KuwoLoginSdk_enterGame("10424");
 }
 
--- frameworks/runtime-src/proj.android/java/com/hdngame/fate/kuwo/KuwoLoginSdkJni.java
+++ frameworks/runtime-src/proj.android/java/com/hdngame/fate/kuwo/KuwoLoginSdkJni.java
@@ -1,7 +1,18 @@
-package com.hdngame.fate;
+package com.hdngame.fate.kuwo;
+
+import cn.kuwo.login.KuwoLoginSdk;
+import com.hdngame.fate.SdkManagerJni;
+
+import java.lang.String;
+import java.lang.System;
 
 /**
  * Created by yuxiao on 15/9/24.
  */
 public class KuwoLoginSdkJni {
+
+    public static void enterGame(String gameShortGame) {
+        System.out.println("KuwoLoginSdkJni.init");
+        KuwoLoginSdk.enterGame(SdkManagerJni.activity, gameShortGame);
+    }
 }
