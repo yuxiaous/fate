e5e42e482b957ea28bd9ca0d6374a4e513ddd5ff
yuxiao <yuxiaous@gmail.com>
2015-10-12 11:30:27 +0800

    no message

--- packAndroid.bat
+++ packAndroid.bat
@@ -1,1 +1,3 @@
+@echo off
+
 tools\cocos2d-console\bin\cocos compile -p android --compile-script 1 %*
---
+++ publishAndroid.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if "%1"=="" (
+	for /f %%t in ('dir /ad/b frameworks\runtime-src\proj.android\targets') do (
+		echo publish target %%t
+		packAndroid.bat -t %%t -m release
+	)
+) else (
+	for %%t in (%*) do if not %%t=="" (
+		echo publish target %%t
+		packAndroid.bat -t %%t -m release
+	)
+)
+
